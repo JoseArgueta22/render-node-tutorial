165f665794a26b9e35b059d995be82dc7c3c4991
JoseArgueta22 <79132116+JoseArgueta22@users.noreply.github.com>
2023-10-25 15:57:13 -0600

    postgres añadido

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import  express  from "express"
+import {config} from 'dotenv'
+import pg from 'pg' 
+
+config()
+
 const app = express()
+const pool = new pg.Pool({
+    connectionString: process.env.DATEBASE_URL,
+    ssl: true 
+})
 app.get('/', (req, res) =>{
     res.send('Hola Mundo')
+})
+
+app.get('/ping', async (req, res) =>{
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
 })
 
 app.listen(3000)
